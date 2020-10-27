08f2928fdd0d95a9107e48a7fe3054edfed21d6b
Aju100 <ajutamang10@outlook.com>
2020-10-27 19:52:25 +0545

    ✅✅✅ Design schemas and DB

---
+++ app/db/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from .database import Base
+
+class NepaliDev(Base):
+    __tablename__ = "NepaliDev"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, primary_key=True),
+    title =  Column(String(50)),
+    actively_job_searching(Boolean, default=False),
+    address= Column(String(30)),
+    experience = Column(Integer),
+    previous_companies = Column(ARRAY(String)),
+    is_intern =  Column(Array(String)),
+    blog_url = Column(String),
+    github_url = Column(String),
+    linkedln_url = Column(String),
+
+    def __init__(self, title, name):
+        self.title = title
+        self.name = name
